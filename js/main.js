2ef8382230fc4345c7611af336191ddfbd98b821
Ritchie Thai <RitchieThai@RitchieThai.com>
2016-01-29 20:38:24 -0800

    Add blue background

--- js/main.js
+++ js/main.js
@@ -4,8 +4,19 @@
 
 	var init = function () {
 		Crafty.init(SCREEN_WIDTH, SCREEN_HEIGHT);
+		makeScreen();
 	}
+
+	var makeScreen = function () {
+		Crafty.e("Screen, 2D, DOM, Color, Mouse")
+			.color('rgb(150, 200, 200)')
+			.attr({w:SCREEN_WIDTH, h:SCREEN_HEIGHT })
+			.bind("MouseMove", function(e) {
+			})
+			.bind("MouseDown", function(e) {
+			});
+	};
+
 
 	init();
 }());
-
