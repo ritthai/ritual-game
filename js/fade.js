a423099318acf977b85bca2b82f4d04e53d0e2a0
Theodore Abshire <TheodoreAbshire@Gmail.com>
2016-01-31 11:02:08 -0800

    Fixed a bug with the splash screen

--- js/fade.js
+++ js/fade.js
@@ -1,8 +1,15 @@
+var fading = false;
+
 var fade = function()
 {
+	if (fading)
+		return; //it's already faing
+	
+	fading = true;
+	
 	var op = 1;
 	var timer = setInterval(function () {
-		if (op < 0.1)
+		if (op < 0.05)
 		{
 			clearInterval(timer);
 			document.getElementById('splashscreen').hidden = true;
@@ -10,5 +17,5 @@ var fade = function()
 		document.getElementById('splashimage').style.opacity = op;
 		document.getElementById('splashimage').style.filter = 'alpha(opacity=' + (op * 100) + ')';
 		op -= op * 0.1;
-	}, 50);
+	}, 30);
 }
